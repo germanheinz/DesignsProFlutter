78af2928a8437089bd913fee96d515ea39367678
germanheinz <heinz.german@gmail.com>
2020-05-25 12:27:14 +0200

    Designs Backgrounds 2 - borders

---
+++ lib/pages/header_page.dart
@@ -0,0 +1,15 @@
+import 'package:designs_pro/widgets/headers.dart';
+import 'package:flutter/material.dart';
+
+class HeadersPage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // body: Center(
+      //   child: Text('Header_Page'),
+      // ),
+      body: HeaderCircle(),
+    );
+  }
+}
---
+++ lib/widgets/headers.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+// class HeaderSquare extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 300,
+//       color: Color(0xff615AAB),
+//     );
+//   }
+// }
+class HeaderCircle extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: BoxDecoration(
+        color: Color(0xff615AAB),
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50))
+      ),
+    );
+  }
+}
