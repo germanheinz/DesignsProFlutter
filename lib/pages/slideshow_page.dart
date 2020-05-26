97c29de5c299a372d1aecf68a7e18d4462940020
germanheinz <heinz.german@gmail.com>
2020-05-26 12:51:24 +0200

    Designs SlideShow - Dots

--- lib/pages/slideshow_page.dart
+++ lib/pages/slideshow_page.dart
@@ -7,8 +7,69 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset('assets/svg/undraw_at_home_octe.svg'),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: _Slides()
+            ),
+             _Dots(),
+          ],
+        )
       )
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: <Widget>[
+          SvgPicture.asset('assets/svg/undraw_at_home_octe.svg'),
+          SvgPicture.asset('assets/svg/undraw_cohort_analysis_stny.svg'),
+          SvgPicture.asset('assets/svg/undraw_nature_on_screen_xkli.svg'),
+        ],
+      ),
+    );
+  }
+}
+class _Dots extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+     width: double.infinity,
+     height: 50,
+     child: Row(
+       mainAxisAlignment: MainAxisAlignment.center,
+       children: <Widget>[
+         Dot(),
+         Dot(),
+         Dot(),
+       ],
+     ),
+    );
+  }
+}
+
+class Dot extends StatelessWidget {
+  const Dot({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 10,
+      width: 10,
+      margin: EdgeInsets.symmetric(horizontal: 5),
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        shape: BoxShape.circle,
+        
+      ),
     );
   }
 }
