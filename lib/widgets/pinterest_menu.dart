968f019a96adbe1dd7213c9c91a53d800f97930d
germanheinz <heinz.german@gmail.com>
2020-05-28 13:14:15 +0200

    Designs Pinterest - Menu Design

--- lib/widgets/pinterest_menu.dart
+++ lib/widgets/pinterest_menu.dart
@@ -22,8 +22,51 @@ class PinterestMenu extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-        child: Text('Hola mundo desde menu'),
+        child: _MenuItems(items),
+        width: 250,
+        height: 50,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(100)),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              color: Colors.black,
+              blurRadius: 9,
+              spreadRadius: -5
+            )
+          ]
+        ),
       ),
+    );
+  }
+}
+
+class _MenuItems extends StatelessWidget {
+  
+  final List<PinterestButton> menuItems;
+
+  _MenuItems(this.menuItems);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(menuItems.length, (index) => _PinterestMenuButton(index, menuItems[index]))
+    );
+  }
+}
+
+class _PinterestMenuButton extends StatelessWidget {
+  
+  final int index;
+  final PinterestButton item;
+
+  _PinterestMenuButton(this.index, this.item);
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(item.icon),
     );
   }
 }
