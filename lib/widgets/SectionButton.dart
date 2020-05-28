3b8c68904f9ff453be9ab5b762b56a1a70685257
germanheinz <heinz.german@gmail.com>
2020-05-28 21:52:35 +0200

    Designs Sports Layout - Section Button

--- lib/widgets/SectionButton.dart
+++ lib/widgets/SectionButton.dart
@@ -5,7 +5,23 @@ class SectionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return _BackgroundSectionButton();
+    return Stack(
+      children: [
+         _BackgroundSectionButton(),
+         Row(
+           mainAxisAlignment: MainAxisAlignment.center,
+           
+           children: [
+              SizedBox(height: 140, width: 40),
+              Icon(Icons.music_video,color: Colors.white, size: 50),
+              SizedBox(width: 20),
+              Expanded(child: Text('Testing', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),)),
+              Icon(Icons.arrow_forward,color: Colors.white, size: 35),
+              SizedBox(width: 40),
+           ],
+         )
+      ],
+    );
   }
 }
 
@@ -17,6 +33,22 @@ class _BackgroundSectionButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15),
+            child: Stack(
+            children: [
+              Positioned(
+                right: -20,
+                top: -20,
+                child: Icon(Icons.music_video, size: 100, color: Colors.white.withOpacity(0.2),),
+                // top: 30,
+                // left: 278,
+              )
+            ],
+          ),
+        ),
+
         width: double.infinity,
         height: 100,
         margin: EdgeInsets.all(20),
