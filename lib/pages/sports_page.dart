96da930c57209b8c6616ffa971926f94eacb43c2
germanheinz <heinz.german@gmail.com>
2020-05-29 11:03:34 +0200

    Designs Sports Layout - Done

--- lib/pages/sports_page.dart
+++ lib/pages/sports_page.dart
@@ -2,34 +2,123 @@
 import 'package:designs_pro/widgets/SectionButton.dart';
 import 'package:designs_pro/widgets/header_sport.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class ItemBoton {
+
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton( this.icon, this.texto, this.color1, this.color2 );
+}
 
 
 class SportsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+      final items = <ItemBoton>[
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+      new ItemBoton( FontAwesomeIcons.carCrash, 'Motor Accident', Color(0xff6989F5), Color(0xff906EF5) ),
+      new ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', Color(0xff66A9F2), Color(0xff536CF6) ),
+      new ItemBoton( FontAwesomeIcons.theaterMasks, 'Theft / Harrasement', Color(0xffF2D572), Color(0xffE06AA3) ),
+      new ItemBoton( FontAwesomeIcons.biking, 'Awards', Color(0xff317183), Color(0xff46997D) ),
+    ];
+    
+
+    List<Widget> itemMap = items.map(
+      (item) => SectionButton(
+        color1: item.color1,
+        color2: item.color2,
+        icon  : item.icon,
+        text  : item.texto,
+        onPress: ()=> {print('Hola')},
+      )).toList();
+    
     return Scaffold(
-    //  body: Header()
-    body: Center(
-      child: SectionButton(),
+    body: Stack(
+      children: [
+        Container(
+            margin: EdgeInsets.only(top: 170),
+            child: ListView(
+            physics: BouncingScrollPhysics(),
+            children: <Widget> [
+              SizedBox(height: 80),
+              ...itemMap
+              // ButtonSectionTemp(),
+              
+              // ButtonSectionTemp(),
+              
+              // ButtonSectionTemp(),
+            ],
+          ),
+        ),
+        _Header()
+      ],
     ),
    );
   }
 }
 
-class Header extends StatelessWidget {
-  const Header({
+class _Header extends StatelessWidget {
+  const _Header({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return HeaderSport(
-      icon: Icons.add,
-      title: 'test',
-      subTitle: 'test2',
-      color1: Colors.grey,
-      color2: Colors.indigo,
+    return HeaderSport(icon: Icons.beach_access, subTitle: 'New Sports',title: 'Sport',);
+  }
+}
+
+class ButtonSectionTemp extends StatelessWidget {
+  const ButtonSectionTemp({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SectionButton(
+          icon: Icons.add,
+          text:  'test',
+          color1: Colors.white,
+          color2: Colors.red,
+          onPress: (){print('Click!');},
+        );
+  }
+}
+
+class Header extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        HeaderSport(
+          icon: Icons.add,
+          title: 'test',
+          subTitle: 'test2',
+          color1: Colors.grey,
+          color2: Colors.indigo,
+        ),
+        Positioned(
+          right: 0,
+          top: 0,
+          // child: FaIcon(FontAwesomeIcons.addressBook, color: Colors.white, size: 200,)
+          child: Icon(Icons.beenhere, color: Colors.white, size: 100),
+        )
+      ],
     );
+    
   }
 }
--- lib/widgets/SectionButton.dart
+++ lib/widgets/SectionButton.dart
@@ -3,32 +3,47 @@ import 'package:flutter/material.dart';
 
 class SectionButton extends StatelessWidget {
 
+  final IconData icon;
+  final Color color1;
+  final Color color2;
+  @required final String text;
+  @required final Function onPress;
+
+  const SectionButton({Key key, this.icon = Icons.add, this.text, this.color1 = Colors.grey, this.color2 = Colors.grey, this.onPress}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-         _BackgroundSectionButton(),
-         Row(
-           mainAxisAlignment: MainAxisAlignment.center,
-           
-           children: [
-              SizedBox(height: 140, width: 40),
-              Icon(Icons.music_video,color: Colors.white, size: 50),
-              SizedBox(width: 20),
-              Expanded(child: Text('Testing', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),)),
-              Icon(Icons.arrow_forward,color: Colors.white, size: 35),
-              SizedBox(width: 40),
-           ],
-         )
-      ],
+    return GestureDetector(
+        onTap: this.onPress,
+        child: Stack(
+        children: <Widget> [
+           _BackgroundSectionButton(icon: this.icon, color1: this.color1, color2: this.color2),
+           Row(
+             mainAxisAlignment: MainAxisAlignment.center,
+             
+             children: [
+                SizedBox(height: 140, width: 40),
+                Icon(this.icon, size: 50, color: color2),
+                SizedBox(width: 20),
+                Expanded(child: Text( this.text, style: TextStyle(color: this.color2, fontWeight: FontWeight.bold, fontSize: 18),)),
+                Icon(Icons.arrow_forward,color: this.color2, size: 35),
+                SizedBox(width: 40),
+             ],
+           )
+        ],
+      ),
     );
   }
 }
 
 class _BackgroundSectionButton extends StatelessWidget {
-  const _BackgroundSectionButton({
-    Key key,
-  }) : super(key: key);
+
+  final IconData icon;
+  final Color color1;
+  final Color color2;
+
+  const _BackgroundSectionButton({Key key, this.icon, this.color1, this.color2}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +56,7 @@ class _BackgroundSectionButton extends StatelessWidget {
               Positioned(
                 right: -20,
                 top: -20,
-                child: Icon(Icons.music_video, size: 100, color: Colors.white.withOpacity(0.2),),
+                child: Icon(this.icon, size: 100, color: Colors.white.withOpacity(0.2),),
                 // top: 30,
                 // left: 278,
               )
@@ -62,8 +77,8 @@ class _BackgroundSectionButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(15.0),
           gradient: LinearGradient(
             colors: <Color>[
-              Colors.red,
-              Colors.orange,
+              this.color1,
+              this.color2,
           ])
          ),
     );
